7bb24f95357c4b3fac1e9f501ad425fabe5d157e
yzeng <yzeng@thoughtworks.com>
2017-04-06 22:05:52 +0800

    George | Welcome Message | Refactor code to delete useless unit test.

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -5,12 +5,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
-
-    @Test
-    public void test() {
-        assertEquals(1, 1);
-    }
-
     @Test
     public void testPrintWelcomeMessage() {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
