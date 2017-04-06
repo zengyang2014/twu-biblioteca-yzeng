9a19f7023ed97d9768a3ade5d4fe58a77ead56f4
yzeng <yzeng@thoughtworks.com>
2017-04-06 22:21:15 +0800

    George | List Books | Write test to make sure user can get the book list. As well, write a constructor to initial book list and a getter.

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,13 +1,29 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+
 public class BibliotecaApp {
+
+    private ArrayList<String> bookList = null;
 
     public static void main(String[] args) {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
         System.out.println(bibliotecaApp.printWelcomeMessage());
     }
 
+    BibliotecaApp(){
+        bookList = new ArrayList<>();
+        bookList.add("Test-driven Development");
+        bookList.add("Refactoring: Improving the Design of Existing Code");
+        bookList.add("Head First Java");
+        bookList.add("Core Java Volume I");
+    }
+
     public String printWelcomeMessage() {
         return "Welcome to Biblioteca";
+    }
+
+    public ArrayList<String> getBookList() {
+        return bookList;
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -2,6 +2,9 @@ package com.twu.biblioteca;
 
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
@@ -11,5 +14,19 @@ public class ExampleTest {
         String actualMessage = bibliotecaApp.printWelcomeMessage();
 
         assertEquals("Welcome to Biblioteca", actualMessage);
+    }
+
+    @Test
+    public void testUserCanGetBooksList() {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        ArrayList<String> bookList = bibliotecaApp.getBookList();
+        ArrayList<String> expectList = new ArrayList<String>(){{
+            add("Test-driven Development");
+            add("Refactoring: Improving the Design of Existing Code");
+            add("Head First Java");
+            add("Core Java Volume I");
+        }};
+
+        assertEquals(expectList, bookList);
     }
 }
