91b718fbba8198f6e5a765238eb036b8f7283ff7
yzeng <yzeng@thoughtworks.com>
2017-04-07 13:39:51 +0800

    George | Book Details | Refactor code to add a new class named book which have name, author, year published fields. And delete useless test to keep code clean.

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,13 +1,10 @@
 package com.twu.biblioteca;
 
-import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import java.util.ArrayList;
 
 public class BibliotecaApp {
 
-    private ArrayList<String> bookList = null;
-    private JTable bookDetail = null;
+    private ArrayList<Book> bookLists = null;
 
     public static void main(String[] args) {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
@@ -17,41 +14,24 @@ public class BibliotecaApp {
 
     BibliotecaApp(){
         initializeBookList();
-        initializeBookDetail();
-    }
-
-    private void initializeBookDetail() {
-        bookDetail = new JTable();
-        DefaultTableModel tableModel = (DefaultTableModel)bookDetail.getModel();
-        tableModel.addColumn("Book Name");
-        tableModel.addColumn("Author");
-        tableModel.addColumn("Year Published");
-        Object[] book1 = {"Test-driven Development", "Kent Beck", "2003"};
-        Object[] book2 = {"Refactoring: Improving the Design of Existing Code", "Martin Fowler", "2012"};
-        Object[] book3 = {"Head First Java", "Kathy Sierra, Bert Bates", "2003"};
-        Object[] book4 = {"Core Java Volume I", "Cay S. Horstmann", "2015"};
-        tableModel.addRow(book1);
-        tableModel.addRow(book2);
-        tableModel.addRow(book3);
-        tableModel.addRow(book4);
     }
 
     private void initializeBookList() {
-        bookList = new ArrayList<>();
-        bookList.add("Test-driven Development");
-        bookList.add("Refactoring: Improving the Design of Existing Code");
-        bookList.add("Head First Java");
-        bookList.add("Core Java Volume I");
+        bookLists = new ArrayList<Book>();
+        bookLists.add(new Book("Test-driven Development", "Kent Beck", "2003"));
+        bookLists.add(new Book("Refactoring: Improving the Design of Existing Code", "Martin Fowler", "2012"));
+        bookLists.add(new Book("Head First Java", "Kathy Sierra, Bert Bates", "2003"));
+        bookLists.add(new Book("Core Java Volume I", "Cay S. Horstmann", "2015"));
     }
 
     public String printBookList() {
-        if(bookList == null) {
+        if(bookLists == null) {
             return null;
         }
 
         String printString = "";
-        for (int i = 0; i < bookList.size(); i++) {
-            printString += bookList.get(i) + "\n";
+        for (int i = 0; i < bookLists.size(); i++) {
+            printString += bookLists.get(i).getBookName() + "\n";
         }
 
         return printString;
@@ -61,7 +41,7 @@ public class BibliotecaApp {
         return "Welcome to Biblioteca";
     }
 
-    public ArrayList<String> getBookList() {
-        return bookList;
+    public ArrayList<Book> getBookLists() {
+        return bookLists;
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -3,9 +3,6 @@ package com.twu.biblioteca;
 
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.ArrayList;
-
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
@@ -21,19 +18,6 @@ public class ExampleTest {
         String actualMessage = bibliotecaApp.printWelcomeMessage();
 
         assertEquals("Welcome to Biblioteca", actualMessage);
-    }
-
-    @Test
-    public void testUserCanGetBooksList() {
-        ArrayList<String> bookList = bibliotecaApp.getBookList();
-        ArrayList<String> expectList = new ArrayList<String>(){{
-            add("Test-driven Development");
-            add("Refactoring: Improving the Design of Existing Code");
-            add("Head First Java");
-            add("Core Java Volume I");
-        }};
-
-        assertEquals(expectList, bookList);
     }
 
     @Test
