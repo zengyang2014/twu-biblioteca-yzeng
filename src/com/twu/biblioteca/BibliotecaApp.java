630fa5cd988947e5b85e9bf2cbf994b1112c7ba9
yzeng <yzeng@thoughtworks.com>
2017-04-07 12:45:08 +0800

    George | Book Details | Add a private field to record the detail of book and initialization of it. Besides, refactor constructor function

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,10 +1,13 @@
 package com.twu.biblioteca;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.util.ArrayList;
 
 public class BibliotecaApp {
 
     private ArrayList<String> bookList = null;
+    private JTable bookDetail = null;
 
     public static void main(String[] args) {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
@@ -13,12 +16,34 @@ public class BibliotecaApp {
     }
 
     BibliotecaApp(){
+        initializeBookList();
+        initializeBookDetail();
+    }
+
+    private void initializeBookDetail() {
+        bookDetail = new JTable();
+        DefaultTableModel tableModel = (DefaultTableModel)bookDetail.getModel();
+        tableModel.addColumn("Book Name");
+        tableModel.addColumn("Author");
+        tableModel.addColumn("Year Published");
+        Object[] book1 = {"Test-driven Development", "Kent Beck", "2003"};
+        Object[] book2 = {"Refactoring: Improving the Design of Existing Code", "Martin Fowler", "2012"};
+        Object[] book3 = {"Head First Java", "Kathy Sierra, Bert Bates", "2003"};
+        Object[] book4 = {"Core Java Volume I", "Cay S. Horstmann", "2015"};
+        tableModel.addRow(book1);
+        tableModel.addRow(book2);
+        tableModel.addRow(book3);
+        tableModel.addRow(book4);
+    }
+
+    private void initializeBookList() {
         bookList = new ArrayList<>();
         bookList.add("Test-driven Development");
         bookList.add("Refactoring: Improving the Design of Existing Code");
         bookList.add("Head First Java");
         bookList.add("Core Java Volume I");
     }
+
     public String printBookList() {
         if(bookList == null) {
             return null;
